a1365f7508a398c50d46510919ea0ab8b3f01f6d
Todimu Jenrola <todimujenrola@gmail.com>
2021-02-16 22:22:38 -0500

    Commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void print () {
     cout << " I love bread and sardine with a cup of tea \n" ;
-    cout << " I also like jollof rice" << endl;
+    cout << " I also like butter chicken and rice" << endl;
 
 }
 
