9533bc4bc26ed0acc29d7ad208eaae4f4cacb58e
Todimu Jenrola <todimujenrola@gmail.com>
2021-03-04 18:30:37 -0500

    Commit

--- findLargest.cpp
+++ findLargest.cpp
@@ -6,7 +6,7 @@ int findLargest( int anArray[], int size);
 
 int main()
 {
-    int bucky[6] = {790067, 454, 123, 697097, 5678, 890};
+    int bucky[6] = {79067, 454, 123, 697097, 5678, 890};
     
     cout << "The largest number in the array is: " << findLargest (bucky, 6) << endl;
     
