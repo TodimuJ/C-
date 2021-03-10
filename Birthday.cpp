af218b192230162d13d105add7f25d3d49ab47bb
Todimu Jenrola <todimujenrola@gmail.com>
2021-03-10 17:52:52 -0500

    Commit

---
+++ Birthday.cpp
@@ -0,0 +1,17 @@
+#include "Birthday.h"
+#include "People.h"
+#include <iostream>
+
+using namespace std;
+
+Birthday::Birthday(int d, int m, int y)
+{
+        date = d;
+        month = m;
+        year = y;//ctor
+};
+
+void Birthday::printDate()
+{
+    cout << date << "/" << month << "/" << year;
+}
